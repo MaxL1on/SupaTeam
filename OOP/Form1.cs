0ba9f16fd0cb210139227b9afe114dde28146947
MaxL1on <MaxL1on@users.noreply.github.com>
2018-02-12 17:51:13 +0300

    нулевой

--- OOP/Form1.cs
+++ OOP/Form1.cs
@@ -16,5 +16,15 @@ namespace OOP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Hello";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
